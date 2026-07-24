ed0f673759e1b0f1689aed7088aeb26bf47597dd
Macbook <huathaiviettuong@MacBook-Air-cua-Macbooktuong2007.local>
2026-07-24 17:29:35 +0700

    lan3

--- src/ui/pages/CategoryPage.h
+++ src/ui/pages/CategoryPage.h
@@ -1,1 +1,14 @@
+#ifndef CATEGORYPAGE_H
+#define CATEGORYPAGE_H
 
+#include <QWidget>
+
+class CategoryPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CategoryPage(QWidget *parent = nullptr);
+};
+
+#endif // CATEGORYPAGE_H
--- src/ui/pages/ReportPage.h
+++ src/ui/pages/ReportPage.h
@@ -1,1 +1,14 @@
+#ifndef REPORTPAGE_H
+#define REPORTPAGE_H
 
+#include <QWidget>
+
+class ReportPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ReportPage(QWidget *parent = nullptr);
+};
+
+#endif // REPORTPAGE_H
