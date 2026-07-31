898d3f4a68e4e3f4cfa35b5245039f0d6a5807fd
vtuong2212 <thuthe788@gmail.com>
2026-07-31 15:27:50 +0700

    Add TransactionDialog header file

--- src/ui/dialogs/TransactionDialog.h
+++ src/ui/dialogs/TransactionDialog.h
@@ -1,1 +1,14 @@
+#ifndef TRANSACTIONDIALOG_H
+#define TRANSACTIONDIALOG_H
 
+#include <QDialog>
+
+class TransactionDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit TransactionDialog(QWidget *parent = nullptr);
+};
+
+#endif // TRANSACTIONDIALOG_H
