ed6fe9b05b05212415d6c4e14951746e7e306572
vtuong2212 <thuthe788@gmail.com>
2026-07-31 15:26:18 +0700

    Add CategoryDialog class definition

---
+++ src/ui/dialogs/CategoryDialog.cpp
@@ -0,0 +1,14 @@
+#ifndef CATEGORYDIALOG_H
+#define CATEGORYDIALOG_H
+
+#include <QDialog>
+
+class CategoryDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CategoryDialog(QWidget *parent = nullptr);
+};
+
+#endif // CATEGORYDIALOG_H
